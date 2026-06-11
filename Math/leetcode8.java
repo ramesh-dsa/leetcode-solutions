98e8c260954e2d8dfbe2dd8f94d731daa3a77181
ramesh-dsa <ramesh.dsa.dev@gmail.com>
2026-06-11 09:59:43 +0530

    Day - 10 of sem leave

---
+++ Math/leetcode8.java
@@ -0,0 +1,14 @@
+//todo           258. Add Digits
+// https://leetcode.com/problems/add-digits/description/
+
+public class leetcode8
+{
+    public static void main(String[] args) {
+        int num = 38 ;
+         if(num==0) 
+          {
+             System.out.println(0); return  ;
+          }  
+       System.out.println(1+(num-1)%9); 
+    }
+}
