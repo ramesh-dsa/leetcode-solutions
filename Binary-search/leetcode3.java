aa16d092ea426606b9cf70dd8ad07b0574a86da9
ramesh-dsa <ramesh.dsa.dev@gmail.com>
2026-06-14 14:07:50 +0530

    day 13 of sem leave

---
+++ Binary-search/leetcode3.java
@@ -0,0 +1,11 @@
+//todo             35. Search Insert Position
+// https://leetcode.com/problems/search-insert-position/description/
+
+/*
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int result = Arrays.binarySearch(nums,target);
+        return (result>=0)?result : Math.abs(result+1) ;
+    }
+}
+*/
