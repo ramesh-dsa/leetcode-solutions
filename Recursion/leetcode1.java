3a83d494ce0e4b1ca4ad76f48db89c9c3f3a916b
ramesh-dsa <ramesh.dsa.dev@gmail.com>
2026-06-19 22:28:32 +0530

    day - 18 of sem leave

---
+++ Recursion/leetcode1.java
@@ -0,0 +1,22 @@
+//todo  509. Fibonacci Number
+// https://leetcode.com/problems/fibonacci-number/description/
+
+/*
+
+// Solved using recursion
+
+class Solution {
+   public static int fibonacci(int n)
+    {
+        if(n<=1)
+          return n ;
+        int flast = fibonacci(n-1);
+        int slast = fibonacci(n-2);
+        return flast+slast; 
+    }
+    public int fib(int n) {
+        return fibonacci(n);
+    }
+}
+
+*/
