3643839e8dfa5f34df6080a46f7db079cef35338
civixapublicservice <rameshmesh55687@gmail.com>
2026-08-17 12:54:56 +0530

    17/08/2026 Contains duplicate II

---
+++ Hash-Map/leetcode8.java
@@ -0,0 +1,29 @@
+//todo   https://leetcode.com/problems/contains-duplicate-ii/description/
+
+/*
+           import java.util.HashMap;
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0 ; i<nums.length ; i++)
+            {
+                if(map.containsKey(nums[i]))
+                {
+                    if( Math.abs(map.get(nums[i])-i)<=k)
+                    {
+                        return true ;
+                    }
+                    else
+                    {
+                        map.put(nums[i],i);
+                    }
+                }
+                else
+                {
+                    map.put([i],i);
+                }
+            }
+        return false;
+    }
+}  
+*/
