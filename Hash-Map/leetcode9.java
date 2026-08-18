7b2c1d66f8165c762a4c3a6abbbd759835030260
civixapublicservice <rameshmesh55687@gmail.com>
2026-08-18 08:07:19 +0530

    18/8/2026 Tuesday 205.Isomorphic Strings

---
+++ Hash-Map/leetcode9.java
@@ -0,0 +1,30 @@
+//todo    https://leetcode.com/problems/isomorphic-strings/description/
+
+/*
+import java.util.HashMap;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+
+        HashMap<Character,Character> map = new HashMap<>();
+        for(int i=0; i <s.length(); i++)
+        {
+            if(map.containsKey(s.charAt(i)))
+            {
+              if(map.get(s.charAt(i)) != t.charAt(i) )
+              {
+                return false ;
+              }
+            }
+            else
+            {
+               if(map.containsValue(t.charAt(i)))
+               {
+                  return false ; 
+               }
+               map.put(s.charAt(i),t.charAt(i));
+            }
+        }
+     return true ;
+    }
+}
+*/
